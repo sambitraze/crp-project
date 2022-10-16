83d1d35df91be1f6ac29ebe5573830d025a1c466
Harsh Raj <harsh.smartters@gmail.com>
2022-10-16 14:49:54 +0530

    Upload Bug Fixed

--- backend/app.py
+++ backend/app.py
@@ -12,41 +12,47 @@ loaded_model = pickle.load(open("modelv1.sav", 'rb'))
 app = Flask(__name__)
 CORS(app)
 
-def convert_obj_to_int(fm):    
+
+def convert_obj_to_int(fm):
     object_list_columns = fm.columns
     object_list_dtypes = fm.dtypes
-    for index in range(0,len(object_list_columns)):
-        if object_list_dtypes[index] == object :
-            fm[object_list_columns[index]] = fm[object_list_columns[index]].apply(lambda x: hash(x))
+    for index in range(0, len(object_list_columns)):
+        if object_list_dtypes[index] == object:
+            fm[object_list_columns[index]] = fm[object_list_columns[index]].apply(
+                lambda x: hash(x))
     return fm
+
 
 @app.route('/', methods=['GET'])
 def hello_world():
-   return 'Hello World'
+    return 'Hello World'
+
 
 @app.route('/', methods=['POST'])
 def hello_world2():
     print("Hehehe", request.files['file'])
     posted_file = request.files['file']
     posted_file.save(secure_filename(posted_file.filename))
-    if(posted_file.filename[-3:] == 'csv'):
+    if posted_file.filename[-3:] == 'csv':
         df_test = pd.read_csv(posted_file)
-    elif(posted_file.filename[-3:]  == 'npy'):
+    elif posted_file.filename[-3:] == 'npy':
         df_test = np.load(posted_file, allow_pickle=True)
         df_test = pd.DataFrame(df_test)
     else:
-        return jsonify({'no_of_ones': 0, 'no_of_zeros': 0, 'error': 'Invalid file type'}) 
-    if(df_test.shape[1] == 15):
-        df_hashed_test= convert_obj_to_int(df_test)    
+        return jsonify({'no_of_ones': 0, 'no_of_zeros': 0, 'error': 'Invalid file type'})
+    if df_test.shape[1] == 15:
+        df_hashed_test = convert_obj_to_int(df_test)
         scaler = StandardScaler()
         fs_test = list(df_hashed_test.columns)
         df_hashed_test[fs_test] = scaler.fit_transform(df_hashed_test[fs_test])
         resp = loaded_model.predict(df_hashed_test)
-    
+
         no_of_ones = resp.size - np.count_nonzero(resp)
         no_of_zeros = np.count_nonzero(resp)
         return jsonify({'no_of_ones': no_of_ones, 'no_of_zeros': no_of_zeros, 'error': 'NA'})
     else:
         return jsonify({'no_of_ones': 0, 'no_of_zeros': 0, 'error': 'Invalid number of columns'})
-if __name__=='__main__':
-    app.run(host="0.0.0.0",port=4000, debug=True)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=4000, debug=True)
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,62 +1,71 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 import { useState } from 'react';
 import axios from 'axios';
-import { FormControl, FormLabel, Input, FormHelperText, Box, Button, WrapItem } from '@chakra-ui/react'
+import {
+    FormControl,
+    FormLabel,
+    Input,
+    FormHelperText,
+    Box,
+    Button,
+    WrapItem,
+} from '@chakra-ui/react';
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(false)
-  const [showResponse, setShowResponse] = useState(false)
-  const [resp, setResponse] = useState('');
-  const [createObjectURL, setCreateObjectURL] = useState(null);
-  const uploadToClient = (event) => {
-    if (event.target.files && event.target.files[0]) {
-      const i = event.target.files[0];
-      setCreateObjectURL(URL.createObjectURL(i));
-      // setCreateObjectURL([...event.target.files[0]]);
-    }
-  };
-  const sendRequest = async () => {
-    setShowResponse(false)
-    setIsLoading(true)
-    const formData = new FormData();
-    // formData.append('file');
-    formData.set('file', createObjectURL);
-    const config = {
-      headers: {
-        'content-type': 'multipart/form-data'
-      }
+    const [isLoading, setIsLoading] = useState(false);
+    const [showResponse, setShowResponse] = useState(false);
+    const [resp, setResponse] = useState(null);
+    const [file, setFile] = useState(null);
+    const uploadToClient = (event) => {
+        if (event.target.files && event.target.files[0]) {
+            setFile({ uploadFile: event.target.files[0] });
+        }
     };
-    const response = await axios.post('http://localhost:4000', formData, config);
-    const data = await response.data
-    console.log(data)
-    setIsLoading(false)
-    setResponse(data)
-    setShowResponse(true)
-  }
-  return (
-    <Box p={24}>
-      <FormControl>
-        <FormLabel>Input File</FormLabel>
-        <Input type='file' onChange={uploadToClient} />
-        <FormHelperText>Please use a csv or npy file only.</FormHelperText>
-      </FormControl>
-      {/* padding */}
-      <Box p={4} />
-      <WrapItem display='flex' alignItems='center'>
-        <Button
-          onClick={sendRequest}
-          isLoading={isLoading}
-          loadingText='Submitting'
-          colorScheme='teal'
-          variant='outline'
-        >Predict</Button>
-      </WrapItem>
-      <Box p={4} />
-      {
-        showResponse ? <Box>{resp}</Box> : <></>
-      }
-    </Box>
-  )
+    const sendRequest = async () => {
+        setShowResponse(false);
+        setIsLoading(true);
+        const formData = new FormData();
+        formData.append('file', file.uploadFile, 'file.png');
+        try {
+            const data = await axios.post('http://127.0.0.1:4000/', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            console.log(data);
+            setIsLoading(false);  
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    return (
+        <Box p={24}>
+            <FormControl>
+                <FormLabel>Input File</FormLabel>
+                <Input type='file' onChange={uploadToClient} />
+                <FormHelperText>
+                    Please use a csv or npy file only.
+                </FormHelperText>
+            </FormControl>
+            {/* padding */}
+            <Box p={4} />
+            <WrapItem display='flex' alignItems='center'>
+                <Button
+                    onClick={sendRequest}
+                    isLoading={isLoading}
+                    loadingText='Submitting'
+                    colorScheme='teal'
+                    variant='outline'
+                >
+                    Predict
+                </Button>
+            </WrapItem>
+            <Box p={4} />
+
+            {/* You cannot a render a object directly, you can only render a specidic value, to render an array or object use map function */}
+            {/* {resp && <Box height={'300px'} width={'300px'}>{resp}</Box>} */}
+        </Box>
+    );
 }
